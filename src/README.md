58d23e1a9f9787d24dd8b6b14fd0c2c192ac94ba
JessicaHora <jessicahora1234@gmail.com>
2024-08-13 18:54:27 -0300

    json to csv

---
+++ src/README.md
@@ -0,0 +1,37 @@
+## CONVERSOR JSON PARA CSV
+
+Desafio para realizar uma conversão de arquivo JSON em um arquivo CSV.
+
+O script principal é o **main.py**.
+
+## Etapas do script:
+
+- Ler o arquivo Json
+- realiza as trasformações necessárias 
+- Salva os resultados em um arquivo CSV
+
+## Como usar:
+
+Para usar o conversor, siga os seguintes passos:
+
+1. Baixe o arquivo JSON fornecido em **docs/base.json**.
+2. Execute o script **main.py** no terminal.
+
+3. O resultado será salvo em um arquivo chamado **base.csv**, na pasta **docs**.
+
+#### Exemplo de Uso:
+
+```
+python main.py
+```
+## Requisitos
+
+O projeto requer as seguintes bibliotecas do python:
+
+- Pandas
+
+Instale usando o PIP:
+
+```
+pip install pandas
+```
--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
 import pandas as pd
+
+
+class JsonToCsvConverter:
+    def __init__(self, json_file_path: str, csv_file_path: str):
+        self.json_file_path = json_file_path
+        self.csv_file_path = csv_file_path
+
+    def convert(self) -> None:
+        """
+        Reads a JSON file from the specified path, converts it to a DataFrame,
+        and exports it to a CSV file at the specified path.
+        """
+        json_dataframe: pd.DataFrame = pd.read_json(self.json_file_path)
+
+        json_dataframe.to_csv(self.csv_file_path, index=False)
+
+
+if __name__ == "__main__":
+    json_file_path = "/home/jessica/desafio_tecnico/docs/base.json"
+    csv_file_path = "/home/jessica/desafio_tecnico/docs/base.csv"
+    converter = JsonToCsvConverter(json_file_path, csv_file_path)
+    converter.convert()
+
+
